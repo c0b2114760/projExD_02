602f95fc29ac1e55e0f5df9b8c2f42ab960ba485
mickey <c0b2114760@edu.teu.ac.jp>
2023-04-25 16:25:40 +0900

    追加機能２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,7 @@ delta = {
         pg.K_RIGHT:(+1, 0)
         }
 
+
   # 練習５
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -36,8 +37,20 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    #kk_img2 = pg.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()  # 練習４
     kk_rct.center = (900, 400)  # 練習４
+
+    # direction = {
+    #             (0, -1): pg.transform.rotozoom(kk_img2, 270, 2.0),
+    #             (-1, -1): pg.transform.rotozoom(kk_img, 305, 2.0),
+    #             (-1, 0): pg.transform.rotozoom(kk_img, 0, 2.0),
+    #             (-1, +1): pg.transform.rotozoom(kk_img, 45, 2.0),
+    #             (0, +1): pg.transform.rotozoom(kk_img2, 90, 2.0),
+    #             (+1, +1): pg.transform.rotozoom(kk_img2, 135, 2.0),
+    #             (+1, 0): pg.transform.rotozoom(kk_img2, 180, 2.0),
+    #             (+1, -1): pg.transform.rotozoom(kk_img2, 225, 2.0)
+    #             }
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習１
     bb_img.set_colorkey((0,0,0))  #練習１
@@ -60,6 +73,7 @@ def main():
         for k, mv in delta.items():
             if key_list[k]:
                 kk_rct.move_ip(mv)
+            
           # 練習５
         if check_bound(screen.get_rect(), kk_rct) != (True, True):
             for k, mv in delta.items():
@@ -75,6 +89,10 @@ def main():
         if not tate:  # 縦方向にはみ出ていたら
             vy *= -1
         screen.blit(bb_img, bb_rct)  # 練習３
+          # 追加機能２
+        if tmr + 1:  # tmrが1増えたら
+            vx *= 1.001  # vxを1.001倍
+            vy *= 1.001  # vyを1.001倍
         if kk_rct.colliderect(bb_rct):  # 練習６
             return
 
