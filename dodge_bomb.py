5f199f6dcc249b878453681d16901db1d2e80f07
mickey <c0b2114760@edu.teu.ac.jp>
2023-04-25 14:32:48 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,7 +15,10 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習１
     bb_img.set_colorkey((0,0,0))  #練習１
     x, y = random.randint(0, 1600), random.randint(0, 900)  #練習２
-    screen.blit(bb_img, [x, y])  #練習２
+    #screen.blit(bb_img, [x, y])  #練習２
+    vx, vy = +1, +1  #練習３
+    bb_rct = bb_img.get_rect()  #練習３
+    bb_rct.center = (x, y)  #練習３
     tmr = 0
 
     while True:
@@ -26,7 +29,8 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])  #練習２
+        bb_rct.move_ip(vx, vy)  #練習３
+        screen.blit(bb_img, bb_rct)  #練習３
 
         pg.display.update()
         clock.tick(1000)
