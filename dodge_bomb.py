f442cb212d07db1e730a9565ad12835855338846
mickey <c0b2114760@edu.teu.ac.jp>
2023-04-25 15:34:38 +0900

    練習問題５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,22 @@ delta = {
         pg.K_RIGHT:(+1, 0)
         }
 
+  # 練習５
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外かを判定し、真理値タプルを返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかとん、または、爆弾SurfaceのRect
+    戻り値：横方向、縦方向のはみ出し判定結果　（画面内：True／画面外：False）
+    """
+
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -39,14 +55,25 @@ def main():
 
         tmr += 1
 
-        key_list = pg.key.get_pressed()  # 練習４
-        for k, mv in delta.items():  # 練習４
-            if key_list[k]:  # 練習４
-                kk_rct.move_ip(mv)  # 練習４
-
+          # 練習４
+        key_list = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_list[k]:
+                kk_rct.move_ip(mv)
+          # 練習５
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_list[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)  #練習４
         bb_rct.move_ip(vx, vy)  # 練習３
+          # 練習５
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)  # 練習３
 
         pg.display.update()
